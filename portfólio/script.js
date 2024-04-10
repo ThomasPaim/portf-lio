24983391e00c6e355e87f807dfe034bb266f9ee2
Thomas Paim <107310282+ThomasPaim@users.noreply.github.com>
2024-04-10 17:06:24 -0300

    Add files via upload

---
+++ portfólio/script.js
@@ -0,0 +1,11 @@
+
+
+const menu = document.getElementById('menu')
+const sidebar = document.querySelector('.sidebar')
+
+
+menu.addEventListener('click', ()=>{
+    sidebar.classList.toggle('mostrar')
+
+})
+
